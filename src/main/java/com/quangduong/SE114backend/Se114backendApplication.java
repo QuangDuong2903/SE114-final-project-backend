3a78c06cc2c50a899a5e742305bc1ca4e3fcc153
Quang Duong <quangduongptsc@gmail.com>
2023-03-23 16:07:01 +0700

    init

---
+++ src/main/java/com/quangduong/SE114backend/Se114backendApplication.java
@@ -0,0 +1,13 @@
+package com.quangduong.SE114backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se114backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se114backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quangduong/SE114backend/Se114backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangduong.SE114backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se114backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
